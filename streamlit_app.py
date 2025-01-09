ee6f05682b6985aaa1e074c6dfcc978edf8628aa
daudisraf <110362243+daudisraf@users.noreply.github.com>
2025-01-09 22:45:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ st.info(' This app uses a machine learning model to predict rainfall')
 
 with st.expander('Data'):
   st.write('**Raw Data**')
-  df = pd.read_csv('https://raw.githubusercontent.com/daudisraf/weatherAUS/refs/heads/main/weatherAUS.csv')
+  df = pd.read_csv('https://raw.githubusercontent.com/daudisraf/weatherAUS/refs/heads/main/cleaned2_weatherAUS.csv')
   df
